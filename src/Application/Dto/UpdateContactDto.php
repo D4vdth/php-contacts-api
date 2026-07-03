ea15dc9de81a66207d3d02c92fcbb7ee3c91338c
D4vdth <davidangarita1100@gmail.com>
2026-07-02 19:39:14 -0500

    fix(application): add id to UpdateContactDto

--- src/Application/Dto/UpdateContactDto.php
+++ src/Application/Dto/UpdateContactDto.php
@@ -8,19 +8,21 @@ namespace App\Application\Dto;
 readonly class UpdateContactDto
 {
     public function __construct(
+        public string $id,
         public string $name,
         public string $lastName,
         public string $email,
         public array $phones
     ){}
 
-    public static function fromArray(array $data): self
+    public static function fromRequest(string $id, array $body): self
     {
         return new self(
-            name: $data['name'] ?? '',
-            lastName: $data['last_name'] ?? '',
-            email: $data['email'] ?? '',
-            phones: $data['phones'] ?? [],
+            id: $id,
+            name: $body['name'] ?? '',
+            lastName: $body['last_name'] ?? '',
+            email: $body['email'] ?? '',
+            phones: $body['phones'] ?? [],
         );
     }
 }
